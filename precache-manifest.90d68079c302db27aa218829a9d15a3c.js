7eb4c950248a086df07a4260fff30d8fe8f7043a
Vishal Jaiswal <jaiswalvishal86@gmail.com>
2020-02-15 15:09:07 +0530

    Updates

---
+++ precache-manifest.90d68079c302db27aa218829a9d15a3c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9e8480bed4622fce2c9cb5a67d7190",
+    "url": "/ar-animations/index.html"
+  },
+  {
+    "revision": "c7082227bc4602502888",
+    "url": "/ar-animations/static/css/main.40d10f10.chunk.css"
+  },
+  {
+    "revision": "c422d8f83a6a256ea89a",
+    "url": "/ar-animations/static/js/2.7aadb8e4.chunk.js"
+  },
+  {
+    "revision": "3cd4b6bc4f51e64816d8a2b69bba1790",
+    "url": "/ar-animations/static/js/2.7aadb8e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7082227bc4602502888",
+    "url": "/ar-animations/static/js/main.fecc665a.chunk.js"
+  },
+  {
+    "revision": "e30f6694988121590ac4",
+    "url": "/ar-animations/static/js/runtime-main.db782950.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/ar-animations/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/ar-animations/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/ar-animations/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/ar-animations/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/ar-animations/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
